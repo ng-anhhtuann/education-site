3d0e1fa28769ec842cace41d0597f3047752c3be
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-09-30 23:33:37 +0700

    Compose source

---
+++ eduhub-api/src/main/java/vn/com/eduhub/EduhubApplication.java
@@ -0,0 +1,13 @@
+package vn.com.eduhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduhubApplication.class, args);
+	}
+
+}
---
+++ eduhub-api/src/test/java/vn/com/eduhub/EduhubApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.eduhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
