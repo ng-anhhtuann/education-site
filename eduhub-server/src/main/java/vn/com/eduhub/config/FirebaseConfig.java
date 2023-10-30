731e05b30b4ced6956f1055d4ebda0995c68d2c8
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-10-30 11:01:01 +0700

    [tuanNA1] Bug fix API store firebase

--- eduhub-server/src/main/java/vn/com/eduhub/config/FirebaseConfig.java
+++ eduhub-server/src/main/java/vn/com/eduhub/config/FirebaseConfig.java
@@ -22,11 +22,14 @@ public class FirebaseConfig {
     @Value("${firebase.bucket-name}")
     private String bucketName;
     
+    @Value("${firebase.class-path}")
+    private String path;
+    
     @SuppressWarnings("deprecation")
     @Bean
     public FirebaseApp initFirebase() throws IOException {
         appName = appName.concat(String.valueOf(System.currentTimeMillis()));
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/eduhub-key.json");
+        FileInputStream serviceAccount = new FileInputStream(path);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket(bucketName)
--- eduhub-server/src/main/java/vn/com/eduhub/service/impl/FileServiceImpl.java
+++ eduhub-server/src/main/java/vn/com/eduhub/service/impl/FileServiceImpl.java
@@ -1,52 +1,73 @@
 package vn.com.eduhub.service.impl;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.ClassPathResource;
 import vn.com.eduhub.dto.master.FileDto;
 import vn.com.eduhub.service.IFileService;
 import vn.com.eduhub.utils.CommonConstant;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.io.File;
 import java.io.IOException;
-import java.util.Date;
+import java.net.URLEncoder;
+import java.io.FileOutputStream;
 import java.util.Objects;
 
 @Service
 @PropertySource("classpath:application.properties")
 public class FileServiceImpl implements IFileService {
 
-    private final Storage storage = StorageOptions.getDefaultInstance().getService();
-
     @Value("${firebase.bucket-name}")
     private String bucketName;
 
+    @Value("${firebase.key-name}")
+    private String path;
+
+    @Value("${firebase.download-url}")
+    private String downloadUrl;
+
+    @Value("${firebase.app-name}")
+    private String appName;
+
+    private final String IMAGE = "image";
+    
+    private final String VIDEO = "video";
+    
     @Override
-    public FileDto uploadImage(MultipartFile file) throws Exception {
+    public FileDto uploadImage(MultipartFile multipartFile) throws Exception {
+
+        String fileName = generateUniqueFileName(multipartFile.getOriginalFilename());
+        if (!isImageFile(Objects.requireNonNull(multipartFile.getOriginalFilename())))
+            throw new Exception(CommonConstant.IMAGE_INVALID);
         try {
-            String fileName = generateUniqueFileName(file.getOriginalFilename());
-            if (isImageFile(Objects.requireNonNull(file.getOriginalFilename()))) {
-                String fileUrl = uploadFile(fileName, file);
-                return new FileDto(fileName, fileUrl);
-            }
-            throw new IllegalArgumentException("Uploaded file is not an image.");
-        } catch (Exception e){
+            File file = this.convertToFile(multipartFile, fileName);
+            String url = this.uploadFile(fileName, file, IMAGE);
+            file.delete();
+            return new FileDto(fileName, url);
+        } catch (Exception e) {
             e.printStackTrace();
             throw new Exception(CommonConstant.PROCESS_FAIL);
         }
     }
 
     @Override
-    public FileDto uploadVideo(MultipartFile file) throws Exception {
+    public FileDto uploadVideo(MultipartFile multipartFile) throws Exception {
+
+        String fileName = generateUniqueFileName(multipartFile.getOriginalFilename());
+        if (!isVideoFile(Objects.requireNonNull(multipartFile.getOriginalFilename())))
+            throw new Exception(CommonConstant.VIDEO_INVALID);
         try {
-            String fileName = generateUniqueFileName(file.getOriginalFilename());
-            if (isVideoFile(Objects.requireNonNull(file.getOriginalFilename()))) {
-                String fileUrl = uploadFile(fileName, file);
-                return new FileDto(fileName, fileUrl);
-            }
-            throw new IllegalArgumentException("Uploaded file is not a video.");
-        } catch (Exception e){
+            File file = this.convertToFile(multipartFile, fileName);
+            String url = this.uploadFile(fileName, file, VIDEO);
+            file.delete();
+            return new FileDto(fileName, url);
+        } catch (Exception e) {
             e.printStackTrace();
             throw new Exception(CommonConstant.PROCESS_FAIL);
         }
@@ -56,37 +77,57 @@ public class FileServiceImpl implements IFileService {
     public boolean deleteFile(String fileName) throws Exception {
         try {
             BlobId blobId = BlobId.of(bucketName, fileName);
+            ClassPathResource serviceAccount = new ClassPathResource(path);
+
+            Storage storage = StorageOptions.newBuilder().setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                    .setProjectId(appName).build().getService();
             return storage.delete(blobId);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw new Exception(CommonConstant.PROCESS_FAIL);
         }
+
     }
 
-    private String generateUniqueFileName(String originalFileName) {
-        return originalFileName + new Date().getTime();
+    private String generateUniqueFileName(String name) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.currentTimeMillis());
+        sb.append(name);
+        return sb.toString();
     }
 
-    private String uploadFile(String fileName, MultipartFile file) throws IOException {
+    private String uploadFile(String fileName, File file, String type) throws IOException {
         BlobId blobId = BlobId.of(bucketName, fileName);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-        byte[] fileData = file.getBytes();
-        Blob blob = storage.create(blobInfo, fileData);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(type).build();
+        ClassPathResource serviceAccount = new ClassPathResource(path);
+        Storage storage = StorageOptions.newBuilder().setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setProjectId(appName).build().getService();
+        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
 
-        return blob.getMediaLink();
+        return String.format(downloadUrl, URLEncoder.encode(fileName, String.valueOf(StandardCharsets.UTF_8)));
+    }
+
+    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File tempFile = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.write(multipartFile.getBytes());
+            fos.close();
+        }
+        return tempFile;
     }
 
     private boolean isImageFile(String fileName) {
         // .jpeg,.jpg,.jpe,.png, .gif, .svg
         // Implement a check for image file extensions
         return fileName.toLowerCase().endsWith(".jpe") || fileName.toLowerCase().endsWith(".jpeg")
-            || fileName.toLowerCase().endsWith(".png") || fileName.toLowerCase().endsWith(".jpg")
-            || fileName.toLowerCase().endsWith(".gif") || fileName.toLowerCase().endsWith(".svg");
+                || fileName.toLowerCase().endsWith(".png") || fileName.toLowerCase().endsWith(".jpg")
+                || fileName.toLowerCase().endsWith(".gif") || fileName.toLowerCase().endsWith(".svg");
     }
 
     private boolean isVideoFile(String fileName) {
         // Implement a check for video file extensions
-        return fileName.toLowerCase().endsWith(".mp4") || fileName.toLowerCase().endsWith(".mov")
-            || fileName.toLowerCase().endsWith(".avi") || fileName.toLowerCase().endsWith(".vmw");
+        return fileName.toLowerCase().endsWith(".mp4") || fileName.toLowerCase().endsWith(".mov") || fileName.toLowerCase().endsWith(".avi")
+                || fileName.toLowerCase().endsWith(".vmw");
     }
+
 }
