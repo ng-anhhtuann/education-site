7b786fa988c605464bd54ead1df26e36698de129
UNIT\tuanna1 <tuanna1@unit.com.vn>
2023-11-10 14:58:19 +0700

    [ng-anhhtuann] Nav bar

--- eduhub-client/src/App.js
+++ eduhub-client/src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
 import Contact from "./pages/contact/Contact";
 import Footer from "./shared/components/common/footer/Footer";
 import Welcome from "./pages/welcome/Welcome";
@@ -12,11 +16,11 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" element={<Welcome/>} />
-          <Route exact path="/home" element={<Home/>} />
-          <Route exact path="/contact" element={<Contact/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/register" element={<Register/>} />
+          <Route exact path="/" element={<Welcome />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
         </Switch>
         <Footer />
       </div>
--- eduhub-client/src/pages/home/Home.js
+++ eduhub-client/src/pages/home/Home.js
@@ -1,52 +1,29 @@
-import React from "react"
-import Heading from "../../shared/components/common/heading/Heading"
-import "./main.css"
+import React from "react";
+import Heading from "../../shared/components/common/heading/Heading";
+import "./main.css";
+import SearchBar from "./SearchBar";
+import PrimarySearchAppBar from "./NavBar";
 
 const Home = () => {
-    const homeAbout = [
-      {
-        id: 1,
-        cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
-        title: "Online Courses",
-        desc: "Enroll in organizational courses to improve your self-taught skills, gain experience, and earn certificates with badges to enhance your professional profile.",
-      },
-      {
-        id: 1,
-        cover: "https://img.icons8.com/ios/80/000000/diploma.png",
-        title: "Earn A Certificates",
-        desc: "By earning certificates for each course, you can enhance your CV with more badges, showcasing your expertise and dedication to continuous learning.",
-      }
-    ]
-    
-    return (
-      <>
-        <section className='aboutHome'>
-          <div className='container flexSB'>
-            <div className='left row'>
-              <img src='./images/about.webp' alt='' />
-            </div>
-            <div className='right row'>
-              <Heading subtitle='LEARN CODE' title='ONLINE CODE LEARNING AND EVERYTHING' />
-              <div className='items'>
-                {homeAbout.map((val, index) => {
-                  return (
-                    <div className='item flexSB' key={index}>
-                      <div className='img'>
-                        <img src={val.cover} alt=''/>
-                      </div>
-                      <div className='text'>
-                        <h2>{val.title}</h2>
-                        <p>{val.desc}</p>
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <SearchBar />
+      <section className="aboutHome">
+        <div className="container flexSB">
+          {/* <div className="left row">
+            <img src="./images/about.webp" alt="" />
+          </div> */}
+          <div className="right row">
+            <Heading
+              subtitle="LEARN CODE"
+              title="ONLINE CODE LEARNING AND EVERYTHING"
+            />
           </div>
-        </section>
-      </>
-    )
-  }
-  
-  export default Home
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
