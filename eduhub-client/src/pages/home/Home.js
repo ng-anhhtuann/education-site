f7785f97716e63ba07163547b4b5cdd1a9e86b18
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-11-11 23:42:11 +0700

    [ng-anhhtuann] Navbar completion

--- eduhub-client/src/pages/home/Home.js
+++ eduhub-client/src/pages/home/Home.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Heading from "../../shared/components/common/heading/Heading";
-import "./main.css";
+import { useNavigate } from "react-router-dom";
 import SearchBar from "./SearchBar";
-import PrimarySearchAppBar from "./NavBar";
+import Navbar from "../../shared/components/common/navbar/NavBar";
+import UserService from "../../shared/service/userService";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const id = sessionStorage.getItem("ID");
+        const response = await UserService.getUserById(id);
+
+        if (response.data.status !== 200) {
+          sessionStorage.clear();
+          navigate("/login");
+        } else {
+          setUserData(response.data.data);
+          sessionStorage.setItem("USER_DATA", JSON.stringify(response.data.data));
+        }
+      } catch (error) {
+        sessionStorage.clear();
+        navigate("/login");
+      }
+    };
+
+    if (sessionStorage.getItem("USER_DATA") === null) {
+      fetchData();
+    } else {
+      const storedUserData = sessionStorage.getItem("USER_DATA");
+      setUserData(JSON.parse(storedUserData));
+    }
+  }, [navigate]);
+
+  if (!userData) {
+    return null;
+  }
+
   return (
     <>
-      <PrimarySearchAppBar />
+      <Navbar userData={userData} />
       <SearchBar />
       <section className="aboutHome">
         <div className="container flexSB">
-          {/* <div className="left row">
+          <div className="left row">
             <img src="./images/about.webp" alt="" />
-          </div> */}
+          </div>
           <div className="right row">
             <Heading
               subtitle="LEARN CODE"
--- eduhub-client/src/shared/service/userService.js
+++ eduhub-client/src/shared/service/userService.js
@@ -1,36 +1,40 @@
-import API from './api';
+import API from "./api";
 
 const UserService = {
 
-    update: (e) => {
-        return API.post('user/edit', e)
-            .then(({ res }) => {
-                console.log({res});
-                setHeadersAndStorage(res.data.id);
-                return res;
-            })
-            .catch((err) => {
-                console.log(err);
-                throw err.response.data;
-            });
-    },
+  update: (e) => {
+    return API.post("user/edit", e)
+      .then((res) => {
+        if (res.data.status === 200) {
+          const id = res.data.data.id;
+          const userData = JSON.stringify(res.data.data);
+          setHeadersAndStorage(id, userData);
+        }
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
+  },
 
-    getUserById: (id) => {
-        return API.get(`user/detail/${id}`)
-            .then(({ res }) => {
-                console.log({res});
-                return res;
-            })
-            .catch((err) => {
-                console.log(err);
-                throw err.response.data;
-            });
-    },
-
+  getUserById: (id) => {
+    return API.get(`user/detail/${id}`)
+      .then((res) => {
+        console.log("call api get detail");
+        console.log(res);
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err.response.data;
+      });
+  },
 };
 
-const setHeadersAndStorage = ({ id }) => {
-    sessionStorage.setItem('ID', id); // Set userId in session storage
-};
+const setHeadersAndStorage = (id, userData) => {
+    sessionStorage.setItem("ID", id); // Set userId in session storage
+    sessionStorage.setItem("USER_DATA", userData);
+  };
 
 export default UserService;
