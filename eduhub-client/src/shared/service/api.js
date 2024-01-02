87fb2710fdcb5c7e1af71eb621f78f36fff61ff3
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2024-01-03 00:25:16 +0700

    [ng-anhhtuann] Change API destination

--- eduhub-client/src/shared/service/api.js
+++ eduhub-client/src/shared/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://eduhub.fly.dev/",
   headers: {
     Accept: "application/json",
   },
