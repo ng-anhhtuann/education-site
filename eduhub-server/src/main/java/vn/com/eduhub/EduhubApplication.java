32fbd578854a7f2d292417a222cec9c4b0b215bd
morphibius <nguyenanhtuan1232@gmail.com>
2026-03-14 11:08:55 +0700

    [ng-anhhtuann] chore: update application entry point configuration

--- eduhub-server/src/main/java/vn/com/eduhub/EduhubApplication.java
+++ eduhub-server/src/main/java/vn/com/eduhub/EduhubApplication.java
@@ -6,15 +6,16 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 @EnableMongoRepositories
-@EnableWebMvc
 public class EduhubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EduhubApplication.class, args);
     }
-
 }
